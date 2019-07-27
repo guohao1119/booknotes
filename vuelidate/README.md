214b38ac4bd340f763e1b43919f21e7019c8f104
郭豪 <guohao@guohaodeMacBook-Pro.local>
2019-07-27 17:34:07 +0800

    vuelidate examples

--- vuelidate/README.md
+++ vuelidate/README.md
@@ -34,10 +34,38 @@
         var validationMixin = window.vuelidate.validationMixin
 
 #### 基础表单
+    首先要将表单中的input通过v-model的形式与data中的数据绑定
+
     对于想要校验的每一个值，需要在validations的选项中创建一个key值。
 
-    
-![Image](https://github.com/guohao1119/booknotes/raw/master/vuelidate/images/vuelidate1.png)
+
+
+##### $v的各个属性值
+![vuelidate1](https://github.com/guohao1119/booknotes/raw/master/vuelidate/images/vuelidate1.png)
+##### $v的name的各个属性值
+![vuelidate2](https://github.com/guohao1119/booknotes/raw/master/vuelidate/images/vuelidate2.png)
         
     
+    可以看到，在组件中配置了validations后，会在组件实例中添加一个$v对象，其中保存着校验相关的数据
+
+    其中，以$开头的属性是vuelidate内置属性，而在validations中配置的每一个属性都会保存在$v中，
+    而这些属性也同样含有以$v开头的内置属性，以及在validations中配置的校验规则
+
+    这些校验规则会影响内置属性的值，随着数据的变化，内置属性的值会根据校验规则而变化，需要根据这些值的变化自定义设置校验提示
+
+#### 不使用v-model
+    当你不想直接修改model时(输入和事件绑定分离)，仍然可以使用。这种情况通常是在使用来自外部的数据，例如Vuex或者props。这种情况下，需要通过在合适的时候调用$touch()方法来手动控制$dirty的值
+
+#### 表单提交
+    在提交表单之前要校验所有要校验的内容是否通过校验。在发送表单提交请求之前，可以通过检查$invalid的状态来实现
+
+#### 关联校验
+    可以通过关联校验器将相关的数据域关联起来。内置的sameAs就是如此。
+
+#### 数据网罗
+    可以设置多个校验器来匹配数据。只要有一个子校验器处于$invalid状态，父级校验器就处于$invalid状态。这对于表单的整体校验很有用（表单提交时判断父级状态即可）。
+
+#### $error vs $anyError
+    有两种方式来判断是否要显示错误信息。弄清楚使用哪种方式更适合是很重要的。既可以使用$error，也可以使用$anyError，或者使用低级别的变量：$dirty或者$anyDirty。文档的例子中主要使用$error，但是选择权在自己手中。
+
         
--- vuelidate/demo/src/App.vue
+++ vuelidate/demo/src/App.vue
@@ -1,10 +1,33 @@
 <template>
   <div id="app">
-    <form>
-      <label for="username">姓名</label>
-      <input type="text" placeholder="请输入姓名" id="username" name="name" v-model.trim="$v.name.$model" />
+    <form @submit.prevent="submit">
+      <label for="name">姓名</label>
+      <input type="text" placeholder="请输入姓名" id="name" name="name" v-model.trim="$v.name.$model" />
       <div class="error" v-if="$v.name.$dirty && !$v.name.required">请输入姓名</div>
       <div class="error" v-if="!$v.name.minLength">至少 {{$v.name.$params.minLength.min}} 个字符.</div>
+      <br />
+      <label for="age">年龄</label>
+      <input type="text" placeholder="请输入年龄" id="age" name="age" v-model.trim="age" @input="setAge($event.target.value)" />
+      <div class="error" v-if="$v.age.$dirty && !$v.age.between">年龄必须介于 {{$v.age.$params.between.min}} 和 {{$v.age.$params.between.max}} 之间</div>
+
+      <br />
+
+      <label for="password">密码</label>
+      <input type="password" placeholder="请输入密码" id="password" name="password" v-model.trim="password" />
+      <div class="error" v-if="$v.password.$dirty && !$v.password.required">请输入密码</div>
+      <div class="error" v-if="!$v.password.minLength">至少8位字符串</div>
+
+
+      <br />
+      <label for="repassword">确认</label>
+      <input type="password" placeholder="请输入密码" id="repassword" name="repassword" v-model.trim="repassword" />
+      <div class="error" v-if="!$v.repassword.sameAsPassword">密码不一致</div>
+
+      <br />
+
+      <button type="submit">提交</button>
+      <br />
+      <p>{{submitTips}}</p>
     </form>
   </div>
 </template>
@@ -12,32 +35,71 @@
 <script>
 
 // 引入要用到的校验规则
-import { required, minLength } from 'vuelidate/lib/validators'
+import { required, minLength, between, sameAs } from 'vuelidate/lib/validators'
 
 export default {
   name: 'app',
   data() {
     return {
       name: '',
+      age: '',
+      submitTips: '',
+      password: '',
+      repassword: ''
     }
   },
   beforeCreate() {
-    console.log(this.$v) //undefined
+    console.log('beforeCreated, this.$v : ' + this.$v) //undefined
   },
   created() {
     console.log(this.$v)
-    console.log(this.$v.name.$dirty)
+    console.log('created, this.$v.name.$dirty : ' + this.$v.name.$dirty) // false
+  },
+  mounted() {
+    console.log('mounted, this.$v.age.$dirty : ' + this.$v.age.$dirty) // false
   },
   validations: {
     name: {
       required,
       minLength: minLength(4)
+    },
+    age: {
+      between: between(20, 40)
+    },
+    password: {
+      required,
+      minLength: minLength(8)
+    },
+    repassword: {
+      sameAsPassword: sameAs('password')
     }
   },
   watch: {
     name() {
-      console.log(this.$v)
-      console.log(this.$v.name.$dirty)
+      console.log(this.$v) //改变input的值，查看$v的各个属性值的变化
+      console.log(this.$v.name.$dirty) // true 在input框输入内容后$dirty变true
+    }
+  },
+  methods: {
+    // 设置年龄
+    setAge(value) {
+      this.submitTips = '';
+      this.age = value;
+      console.log('调用$touch方法之前：');
+      console.log('this.$v.age.$dirty : ' + this.$v.age.$dirty);
+      console.log(this.$v.age.$between);
+      this.$v.age.$touch();
+      console.log('调用$touch方法之后：');
+      console.log('this.$v.age.$dirty : ' + this.$v.age.$dirty);
+      console.log(this.$v.age.$between);
+    },
+    submit() {
+      this.$v.$touch();
+      if (this.$v.$invalid) {
+        this.submitTips = '数据有错哟，请检查一下';
+      } else {
+        this.submitTips = '填写的内容已提交~';
+      }
     }
   },
 }
